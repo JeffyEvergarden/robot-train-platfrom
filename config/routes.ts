32f3f76c8af9693d0de4c2143c78b19fcdb42216
duan <freedom648754076@163.com>
2023-03-22 15:28:24 +0800

    feat: 增加菜单路由

--- config/routes.ts
+++ config/routes.ts
@@ -18,11 +18,47 @@ export default [
     name: '画布demo测试',
     layout: true,
   },
+  //-------导师端
+  {
+    path: '/teacher/paramsManage',
+    component: './teacher-web/params-manage/home',
+    name: '参数管理',
+    layout: true,
+    routes: [
+      {
+        path: '/teacher/paramsManage/userManage',
+        component: './teacher-web/params-manage/user-manage',
+        name: '用户管理',
+      },
+      {
+        path: '/teacher/paramsManage/ruleManage',
+        component: './teacher-web/params-manage/rule-manage',
+        name: '规则管理',
+      },
+      {
+        path: '/teacher/paramsManage/systemManage',
+        component: './teacher-web/params-manage/system-manage',
+        name: '系统管理',
+      },
+    ],
+  },
   // -----学员端
+  {
+    path: '/student/personalInfo',
+    component: './student-web/personal-info',
+    name: '首页',
+    layout: true,
+  },
   {
     path: '/student/course',
     component: './student-web/course',
     name: '学习课程',
+    layout: true,
+  },
+  {
+    path: '/student/learnRecord',
+    component: './student-web/learn-record',
+    name: '学习记录',
     layout: true,
   },
   {
---
+++ src/pages/teacher-web/params-manage/user-manage/index.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>用户管理</div>;
+};
