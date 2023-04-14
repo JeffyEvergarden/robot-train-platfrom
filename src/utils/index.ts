eb26bc4e644181a255583041ea4eced60743cd97
13113193306 <jiahao3306@163.com>
2023-04-14 10:44:04 +0800

    fix: 表单名称去空

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,10 @@ export function formatePercent(val: number): string {
   }
   return '0%';
 }
+
+export const handleKeyPress = (e: any) => {
+  // 如果用户按下了空格或制表符，则防止它们被输入
+  if (e.key === ' ' || e.key === 'Tab') {
+    e.preventDefault();
+  }
+};
