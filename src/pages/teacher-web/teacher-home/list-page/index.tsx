5a15531b15827da70afd84041a61163af8e7921c
liangjiabao <>
2023-05-24 17:21:55 +0800

    fix: 导师端首页联调问题修复

--- src/pages/teacher-web/teacher-home/list-page/index.tsx
+++ src/pages/teacher-web/teacher-home/list-page/index.tsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState, useImperativeHandle } from 'react';
 import style from './style.less';
 import { Tag, Empty } from 'antd';
 import { Link, history } from 'umi';
-import { useCourseModel } from '../../../student-web/course/model';
 import coursePic from '@/asset/image/course-pic.png';
-import Condition from '@/components/Condition';
 import Process from '../component/process';
 
 import { formatePercent } from '@/utils';
@@ -45,52 +43,43 @@ const ListPage: React.FC<any> = (props: any) => {
               firstInfoName = '考试人数';
             }
             return (
-              <Link
-                target="blank"
-                key={index}
-                to={
-                  {
-                    // pathname: `/front/student/course/detail`,
-                    // search: `?taskId=${item.taskId}&tab=${type + 1}`,
-                  }
-                }
-              >
-                <div className={style['card-container']}>
-                  <div className={style['top-container']}>
-                    <img src={coursePic} className={style['icon']} />
-                    <div>
-                      <div className={style['title-container']}>
-                        <span className={style['task-title']}>{taskName}</span>
-                        <div style={{ marginLeft: '8px' }}>{tagType}</div>
-                      </div>
-                      <div className={style['process-container']}>
+              <div className={style['card-container']} key={index}>
+                <div className={style['top-container']}>
+                  <img src={coursePic} className={style['icon']} />
+                  <div className={style['top-rightContainer']}>
+                    <div className={style['title-container']}>
+                      <span className={style['task-title']}>{taskName}</span>
+                      <div style={{ marginLeft: '8px' }}>{tagType}</div>
+                    </div>
+                    <div className={style['process-container']}>
+                      <div className={style['process-bar']}>
                         <Process percent={completeRate} />
-                        <div className={style['process-text']}>
-                          完成进度：{formatePercent(item.completeRate)}
-                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className={style['bottom-container']}>
-                    <div className={style['counter-info-container']}>
-                      <div className={style['counter-info-l-text']}>{firstInfoName}</div>
-                      <div className={style['counter-info-r-text']}>{trainUser}</div>
-                    </div>
-                    <div className={style['counter-info-container']}>
-                      <div className={style['counter-info-l-text']}>人均训练次数</div>
-                      <div className={style['counter-info-r-text']}>{timesAvg}</div>
-                    </div>
-                    <div className={style['counter-info-container']}>
-                      <div className={style['counter-info-l-text']}>完成人数</div>
-                      <div className={style['counter-info-r-text']}>{completeCount}</div>
-                    </div>
-                    <div className={style['counter-info-container']}>
-                      <div className={style['counter-info-l-text']}>平均分</div>
-                      <div className={style['counter-info-r-text']}>{scoreAvg}</div>
+                      <div className={style['process-text']}>
+                        完成进度：{formatePercent(item.completeRate)}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </Link>
+                <div className={style['bottom-container']}>
+                  <div className={style['counter-info-container']}>
+                    <div className={style['counter-info-l-text']}>{firstInfoName}</div>
+                    <div className={style['counter-info-r-text']}>{trainUser}</div>
+                  </div>
+                  <div className={style['counter-info-container']}>
+                    <div className={style['counter-info-l-text']}>人均训练次数</div>
+                    <div className={style['counter-info-r-text']}>{timesAvg}</div>
+                  </div>
+                  <div className={style['counter-info-container']}>
+                    <div className={style['counter-info-l-text']}>完成人数</div>
+                    <div className={style['counter-info-r-text']}>{completeCount}</div>
+                  </div>
+                  <div className={style['counter-info-container']}>
+                    <div className={style['counter-info-l-text']}>平均分</div>
+                    <div className={style['counter-info-r-text']}>{scoreAvg}</div>
+                  </div>
+                </div>
+              </div>
             );
           })}
         </div>
